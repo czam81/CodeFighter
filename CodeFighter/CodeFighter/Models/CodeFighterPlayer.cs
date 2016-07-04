0ba7c165ab94a3490ca19bb90ba68c4f74b58a62
Christian Adolfo Zamudio Nakamura <czamudio30@hotmail.com>
2016-07-04 13:11:03 -0500

    adding player logic

--- CodeFighter/CodeFighter/Models/CodeFighterPlayer.cs
+++ CodeFighter/CodeFighter/Models/CodeFighterPlayer.cs
@@ -7,7 +7,88 @@ namespace CodeFighter.Models
 {
     public class CodeFighterPlayer
     {
-        string name;
-        string role;
+        public string name
+        {
+            get; set;
+        }
+        public string role
+        {
+            get; set;
+        }
+        public int energy
+        {
+            get; set;
+        }
+        public int life
+        {
+            get; set;
+        }
+        private int maxLife;
+        private int maxEnergy;
+        private int healDamage = 50;
+        public CodeFighterPlayer(string _name, string _role)
+        {
+            name = _name;
+            role = _role;
+        }
+
+        public void resetLifeForRound(int? _maxLife, int? _maxEnergy)
+        {
+            life = _maxLife.HasValue ? _maxLife.Value : 200;
+            energy = 0;
+            maxLife = _maxLife.HasValue ? _maxLife.Value : 100;
+            maxEnergy = _maxEnergy.HasValue ? _maxEnergy.Value : 100;
+        }
+
+        public void receivePunch()
+        {
+            receiveDamage(10);
+        }
+        public void receiveKick()
+        {
+            receiveDamage(20);
+        }
+        public void receiveSpecial()
+        {
+            receiveDamage(30);
+        }
+
+        public void punch()
+        {
+            receiveEnergy(5);
+        }
+        public void kick()
+        {
+            receiveEnergy(8);
+        }
+        public void special()
+        {
+            if (energy == maxEnergy)
+            {
+                loseEnergy(maxEnergy);
+                receiveEnergy(15);
+            }
+        }
+
+        private void heal()
+        {
+            if (energy == maxEnergy)
+            {
+                life = life + healDamage >= maxLife ? maxLife : life + healDamage;
+                loseEnergy(maxEnergy);
+            }
+        }
+        private void receiveDamage(int damage)
+        {
+            life = life - damage <= 0 ? 0 : life - damage;
+        }
+        private void receiveEnergy(int energyAdded)
+        {
+            energy = energy + energyAdded >= maxEnergy ? maxEnergy : energy + energyAdded;
+        }
+        private void loseEnergy(int energySpent)
+        {
+            energy = energy - energySpent <= 0 ? 0 : energy - energySpent;
+        }
     }
 }
